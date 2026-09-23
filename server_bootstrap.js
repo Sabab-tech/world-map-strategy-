e60b6cbb6a71a3619ffb3c1b8ea7bea447526691
Sabab-tech <fazlealahi1968@gmail.com>
2026-09-23 20:22:44 +0600

    Boot restored resource ministry engine before opponent autonomy

--- server_bootstrap.js
+++ server_bootstrap.js
@@ -106,6 +106,7 @@ const CANONICAL_AI_SCRIPTS = Object.freeze([
   'omega_language_batch03_semantic_extension.js',
   'omega_country_semantic_bridge.js',
   'omega_resource_semantic_bridge.js',
+  'resource_ministry_engine.js',
   'omega_minister_runtime_v2.js',
   'omega_ai_integrity_layer.js',
   'omega_ai_integrity_canonical_bridge.js',
