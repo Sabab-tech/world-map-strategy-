f80f19231de980c4fd6a829044933cdb8988132a
Sabab-tech <fazlealahi1968@gmail.com>
2026-09-25 23:05:20 +0600

    feat(resource): wire global mine pipeline server_bootstrap.js

--- server_bootstrap.js
+++ server_bootstrap.js
@@ -107,6 +107,8 @@ const CANONICAL_AI_SCRIPTS = Object.freeze([
   'omega_country_semantic_bridge.js',
   'omega_resource_semantic_bridge.js',
   'resource_ministry_engine.js',
+  'omega_resource_part04_identity_runtime.js',
+  'omega_resource_part05_reserve_extraction_runtime.js',
   'omega_minister_runtime_v2.js',
   'omega_ai_integrity_layer.js',
   'omega_ai_integrity_canonical_bridge.js',
